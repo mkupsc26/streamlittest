aec8e1b57e1cf88ec90c22e304d8e320b31646f0
Madeleine Nielsen <168845829+Ms-Nielsen@users.noreply.github.com>
2025-06-02 09:30:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
-st.write("Testing 1 2 3")
+st.write("You have entered a dark forest, would you like to take the left path or the right?")\
+
+if st.button("Turn Left"):
+  st.write("You turned left")
+if st.button("Turn Right"):
+  st.write("You turned right")
