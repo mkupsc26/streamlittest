322d2bc7cd3753bfc8ca7297d035d76f4f0d90f7
mkupsc26 <mkupsc26@cohassetk12.org>
2025-06-03 11:40:21 -0400

    Update dogage.py

--- dogage.py
+++ dogage.py
@@ -4,7 +4,6 @@ def calc():
   st.write("The age of", dogname, "in human years is,", age)
 dogname = st.text_input("Enter your dog's name :")
 age = st.number_input("Enter your dog's age: ")
-st.button("Calculate")
 if st.button("Calculate"):
   calc()
 
