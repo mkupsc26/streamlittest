cb8d22aca7b093ae5434bd2c52b75f45c99ad2ba
mkupsc26 <mkupsc26@cohassetk12.org>
2025-06-03 11:41:15 -0400

    Update dogage.py

--- dogage.py
+++ dogage.py
@@ -1,9 +1,9 @@
 import streamlit as st
+dogname = st.text_input("Enter your dog's name :")
+age = st.number_input("Enter your dog's age: ")
 def calc():
   age = age * 7
   st.write("The age of", dogname, "in human years is,", age)
-dogname = st.text_input("Enter your dog's name :")
-age = st.number_input("Enter your dog's age: ")
 if st.button("Calculate"):
   calc()
 
