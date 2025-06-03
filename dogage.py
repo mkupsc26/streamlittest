5fc189d1ed062657c6fe6cb4683a5bb164af6812
mkupsc26 <mkupsc26@cohassetk12.org>
2025-06-03 11:24:25 -0400

    Update dogage.py

--- dogage.py
+++ dogage.py
@@ -1,3 +1,14 @@
-main()
-dogname = st.text_input("Enter your dog's name :")
-age = st.number_input("Enter your dog's age: ")
+def main():
+  inputs()
+
+def inputs(): 
+  global dogname
+  global age
+  dogname = st.text_input("Enter your dog's name :")
+  age = st.number_input("Enter your dog's age: ")
+  return dogname
+  return age
+def calculations():
+  age = age * 7
+  st.write(
+  
