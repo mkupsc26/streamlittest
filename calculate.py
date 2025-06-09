62d28a74cc82cf49bfece25904e32b8631bf6562
mkupsc26 <mkupsc26@cohassetk12.org>
2025-06-09 10:41:17 -0400

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
-x1 = 
+x1 = st.number_input("Enter the first X coordinate ")
+y1 = st.number_input("Enter the first Y coordinate ")
+x2 = st.number_input("Enter the second X coordinate ")
+y2 = st.number_input("Enter the second Y coordinate ")
+
+
