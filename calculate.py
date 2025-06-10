6691ce5ba86054d9f2dd3c4df4c2b3e99608510f
mkupsc26 <mkupsc26@cohassetk12.org>
2025-06-10 11:35:23 -0400

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -4,6 +4,11 @@ y1 = st.number_input("Enter the first Y coordinate ")
 x2 = st.number_input("Enter the second X coordinate ")
 y2 = st.number_input("Enter the second Y coordinate ")
   
+def calcMid(x1,x2,y1,y2):
+  xMid = (x1+x2)/2
+  yMid = (y1+y2)/2
+  midpoint = ("(" + str(xMid) + ", " + str(yMid) + ")")
+  return midpoint  
   
 if st.button("Calculate midpoint: "):
   x1=int(x1)
@@ -13,12 +18,6 @@ if st.button("Calculate midpoint: "):
   mid = calcMid(x1,x2,y1,y2)
   st.write("Midpoint:", mid)
   
-#if st.button("Calculate distance: "):
-  #distance = calculate
   
-def calcMid(x1,x2,y1,y2):
-  xMid = (x1+x2)/2
-  yMid = (y1+y2)/2
-  midpoint = ("(" + str(xMid) + ", " + str(yMid) + ")")
-  return midpoint
+
 
