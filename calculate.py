ce807359e98b99b9b760144cc6de4603ab998cd7
mkupsc26 <mkupsc26@cohassetk12.org>
2025-06-10 11:30:06 -0400

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -1,20 +1,21 @@
 import streamlit as st
-
-x1 = st.number_input("Enter the first X coordinate ")
-y1 = st.number_input("Enter the first Y coordinate ")
-x2 = st.number_input("Enter the second X coordinate ")
-y2 = st.number_input("Enter the second Y coordinate ")
-
-def calcMid(x1,x2,y1,y2):
-  xMid = (x1+x2)/2
-  yMid = (y1+y2)/2
-  return yMid
-  return xMid
-
-if st.button("Calculate midpoint: "):
-  mid == calcMid(x1,x2,y1,y2)
-  st.write("Midpoint:", mid)
-
-if st.button("Calculate distance: "):
-  distance = calculate
+def main():
+  x1 = st.number_input("Enter the first X coordinate ")
+  y1 = st.number_input("Enter the first Y coordinate ")
+  x2 = st.number_input("Enter the second X coordinate ")
+  y2 = st.number_input("Enter the second Y coordinate ")
   
+  
+  if st.button("Calculate midpoint: "):
+    mid == calcMid(x1,x2,y1,y2)
+    st.write("Midpoint:", mid)
+  
+  if st.button("Calculate distance: "):
+    distance = calculate
+  
+  def calcMid(x1,x2,y1,y2):
+    xMid = (x1+x2)/2
+    yMid = (y1+y2)/2
+    midpoint = ("(" + str(xMid) + ", " + str(yMid) + ")")
+    return midpoint
+main()
